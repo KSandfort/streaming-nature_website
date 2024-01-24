0ae1c6efb4e1c314edd007dedce1c4cdbf73ca95
KSandfort <konstantin.sandfort@gmail.com>
2024-01-24 14:23:07 +0100

    Fixed youtube player

--- src/components/YouTubePlayer.tsx
+++ src/components/YouTubePlayer.tsx
@@ -1,26 +1,40 @@
+import { useEffect, useState } from "react";
 import YouTube from "react-youtube";
 
 const YouTubePlayer = () => {
-  let currentURL = "";
+  // Adapt the refresh interval
+  const refrestIntervalMs: number = 10000;
 
-  const fetchStreamURL = async () => {
-    try {
-      const response = await fetch(
-        "http://127.0.0.1:8000/v1/streams?score_number=0"
-      );
+  const [counter, setCounter] = useState(0);
+  const [videoURL, setVideoURL] = useState<string>("initialVideoId");
 
-      if (!response.ok) {
-        throw new Error("Network response was not ok.");
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCounter((prevCounter) => prevCounter + 1);
+      console.log("Change stream " + counter);
+    }, refrestIntervalMs);
+
+    const fetchStreamURL = async () => {
+      try {
+        const response = await fetch(
+          "http://127.0.0.1:8000/v1/streams?score_number=0"
+        );
+
+        if (!response.ok) {
+          throw new Error("Network response was not ok.");
+        }
+        const data = await response.text();
+        console.log("Fetched Data:", data);
+        setVideoURL(data.toString());
+      } catch (error) {
+        console.error("Error fetching data:", error);
       }
+    };
 
-      const data = await response.text();
-      console.log("Fetched Data:", data);
-      currentURL = data.toString();
-      return data;
-    } catch (error) {
-      console.error("Error fetching data:", error);
-    }
-  };
+    fetchStreamURL();
+
+    return () => clearInterval(interval);
+  }, [counter]);
 
   const opts = {
     height: "540",
@@ -32,10 +46,9 @@ const YouTubePlayer = () => {
 
   const onReady = (event: { target: { playVideo: () => void } }) => {
     event.target.playVideo(); // Autoplay the video when the player is ready
-    fetchStreamURL();
   };
 
-  return <YouTube videoId={currentURL} opts={opts} onReady={onReady} />;
+  return <YouTube videoId={videoURL} opts={opts} onReady={onReady} />;
 };
 
 export default YouTubePlayer;
