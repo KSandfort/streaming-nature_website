b6e0f0cdea549f5d4a4df1e879a04153ab5c25f8
KSandfort <konstantin.sandfort@gmail.com>
2024-06-24 11:50:59 +0200

    added weather and location api fetching

--- src/components/MetaDataFrame.tsx
+++ src/components/MetaDataFrame.tsx
@@ -1,10 +1,34 @@
 import "../../scss/custom.scss";
+import { useEffect, useState } from "react";
 
 function MetaDataFrame() {
+  const [data, setData] = useState<string>("");
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("PLACE API URL HERE");
+        if (!response.ok) {
+          throw new Error(
+            "HTTP error -> Weather and Location API request not successful"
+          );
+        }
+        const result = await response.text();
+        setData(result);
+      } catch (error) {
+        console.error("Error!", error);
+      }
+    };
+
+    fetchData();
+    const intervalID = setInterval(fetchData, 5000);
+    return () => clearInterval(intervalID);
+  }, []);
+
   return (
     <div className="container">
       <div className="row">
-        <p>Location Info</p>
+        <p>{data}</p>
       </div>
       <div className="row">
         <p>Weather Info</p>
